13be857c1319e65f8579b51c1e8194d01cc240e9
Morten Skipenes Østrem <47166099+Draugmood@users.noreply.github.com>
2024-05-27 10:57:04 +0000

    Forgot to edit a variable

--- main.py
+++ main.py
@@ -104,7 +104,7 @@ def get_enemy_position_around_player(player_position, distance):
 async def main():
 
   background = pygame.Surface(glb.SCREEN.get_size()).convert()
-  background.fill(glb.SLATEGREY)
+  background.fill(glb.DARKSLATEGREY)
 
   screen_center = vec(glb.SCREEN.get_size()) / 2
 
